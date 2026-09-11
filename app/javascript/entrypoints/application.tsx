05c5719d4394f6d27e2acaa64e60960fb80e9fb5
Peter Lemiszki <plemiszki@gmail.com>
2026-09-11 10:45:50 -0400

    create app

---
+++ app/javascript/entrypoints/application.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const appDiv = document.getElementById("app");
+const root = createRoot(appDiv);
+root.render(<p>Hello from React!</p>);
